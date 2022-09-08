c889aeb63cd87d01441199e6e376b6f3cd2ef2b3
Sol Zilberman <solzilbermancode@gmail.com>
2022-09-07 21:15:20 -0400

    updated workflow

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+(async function run() {
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASSWORD
+        }
+      });
+      
+      await transporter.sendMail({
+        from: process.env.DAILY_REPORT_FROM,
+        to: process.env.DAILY_REPORT_TO,
+        subject: 'Daily Report',
+        text: `
+          Daily Report
+        `,
+        html: `
+          <h1>Daily Report</h1>
+        `,
+      });
+})();
